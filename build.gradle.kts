3fdbd9a109ef9eff4accb48084f7344db3005c19
kadenscott <github.com@kaden.sh>
2022-08-13 11:44:50 -0400

    chore: setup gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("java")
+    `maven-publish`
+    id("io.papermc.paperweight.userdev")
+    id("com.github.johnrengelman.shadow")
+    id("org.checkerframework")
 }
 
 group = "cafe.navy"
@@ -7,11 +11,19 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://jitpack.io")
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.broccol.ai/releases")
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    paperDevBundle("1.19.1-R0.1-SNAPSHOT")
+    compileOnly(libs.cloud.core)
+    compileOnly(libs.cloud.paper)
+    compileOnly(libs.corn.minecraft.paper) {
+        exclude(group="io.papermc.paper", module="paper-api")
+    }
 }
 
 tasks.getByName<Test>("test") {
